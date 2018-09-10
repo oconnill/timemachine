bca9de602c01499a8d60ff3b6bbde71f28e5d717
oconnill <doconnelldesign@gmail.com>
2018-09-10 14:50:26 -0600

    Fix typo in introduction to user guide

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the day of your birth");
+
+            int day = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the month of your birth");
+
+            int month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the year of your birth");
+
+            int year = int.Parse(Console.ReadLine());
+
+            DateTime dateValue = new DateTime(year, month, day);
+
+            Console.WriteLine(dateValue.ToString("ddd"));
+
+
+
+        }
+    }
+}
